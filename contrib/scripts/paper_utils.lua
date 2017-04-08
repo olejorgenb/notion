9d596048a7288461d5d260f5e375dc8ab289ce41
Ole Jørgen Brønner <olejorgenb@yahoo.no>
2017-04-08 18:18:43 +0200

    Utils

--- contrib/scripts/paper_utils.lua
+++ contrib/scripts/paper_utils.lua
@@ -4,6 +4,199 @@ function string.has_prefix(str, prefix)
     return string.sub(str, 1, string.len(prefix)) == prefix
 end
 
+function table_to_string(tab, depth_limit, param)
+
+    function slice(tbl, first, last, step)
+        local sliced = {}
+
+        for i = first or 1, last or #tbl, step or 1 do
+            sliced[#sliced+1] = tbl[i]
+        end
+
+        return sliced
+    end
+
+    function indent(levels, depth)
+        local s = ""
+        for i, c in ipairs(levels) do
+            if i >= depth then break end
+            s=s..c.." "
+        end
+        return s
+        -- return table.concat(slice(levels, 1, depth), " ")
+    end
+
+    function table_to_linestring(tab)
+        local kvs = {}
+        for k,v in pairs(tab) do
+            table.insert(kvs, k..": "..tostring(v))
+        end
+        return "{ "..table.concat(kvs, ", ").." }"
+    end
+
+    function notion_inliner(k, v)
+        --- Returns the inlined value and a set of keys consumed
+        if k == "geom" then
+            return table_to_linestring(v), v
+        elseif v["type"] then
+            if v.name then
+                return v["type"]..": "..v.name, { type=true, name=true }
+            else
+                return v["type"], { type=true }
+            end
+        else 
+            return nil
+        end
+    end
+
+    local default_param = {
+        tostring_fn = function(v)
+            if type(v) == "userdata" and v.__typename
+            then return "["..v.__typename.."]"
+            else return tostring(v)
+            end
+        end,
+        inliner = notion_inliner,
+        filter = function(k, v) return true end,
+    }
+
+    param = table.join(param or {}, default_param)
+
+    local lines = {}
+
+    -- indent indicators indexed by `depth`
+    local levels = {}
+
+    function filter_table(t, f)
+        local res={}
+        for k, v in pairs(t) do
+            res[k]=f(k, v)
+        end
+        return res
+    end
+
+    function recurse(tab, depth, skip)
+        local more = ((depth_limit == nil) or depth < depth_limit)
+
+        function skip_filter_p(k, v)
+            if skip and skip[k] then
+                return nil
+            end
+            if not param.filter(k, v) then
+                debug.print_line("Skipping "..k)
+                return nil
+            end
+            return v
+        end
+
+        debug.print_line(tostring(tab == nil))
+        local filtered = filter_table(tab, skip_filter_p)
+
+        for k,v in pairs(filtered) do
+            local entry = indent(levels, depth).."+-"..k..": "
+
+            if type(v) == "table" then
+                local inlined, consumed_keys = param.inliner(k, v)
+                if not inlined then
+                    if not more then
+                        inlined = "{...}"
+                    elseif not next(v) then
+                        inlined = "{}"
+                    end
+                end
+                if inlined then
+                    entry = entry..inlined
+                end
+                table.insert(lines, entry)
+
+                indent_char = " "
+                debug.print_line(tostring(next(filtered, k)).." "..k)
+                if next(filtered, k) then
+                   indent_char = "|"
+                end
+                levels[depth] = indent_char
+                if more then
+                    recurse(v, depth+1, consumed_keys)
+                end
+            else
+                entry = entry..param.tostring_fn(v)
+                table.insert(lines, entry)
+            end
+        end
+    end
+
+    recurse(tab, 1)
+
+    return table.concat(lines, "\n")
+end
+
+--- Debug
+
+-- (Ab)use loss debug module as namespace if it's present
+if debug == nil then
+    debug = {}
+end
+
+-- Or simply write to stderr/stdout?
+debug.output_file_name = "~/.notion/debug"
+debug.output_file = nil
+
+debug.print = nil
+
+function debug.write(...)
+    if not debug.output_file then
+        debug.output_file = io.open(glob(debug.output_file_name), "a+")
+    end
+    for i, str in ipairs(table.pack(...)) do
+        debug.output_file:write(str)
+    end
+    debug.output_file:flush()	
+end
+
+function debug.printf(formatstr, ...)
+    formatstr = tostring(formatstr)
+    debug.write(string.format(formatstr, ...), "\n")
+end
+
+--- dump all local variables to the debug file
+function debug.dump_locals()
+    local i = 1
+    while i < 100 do
+        local name, value = debug.getlocal(2, i)
+        if not name then break end
+        debug.print_line(name.."\t"..tostring(value))
+        i = i+1
+    end
+end
+
+function debug.print_line(str)
+    debug.write((str or "nil").."\n")
+end
+
+function debug.log(formatstr, ...)
+    debug.write(os.date("%H:%M:%S"), " ",
+                string.format(formatstr, ...), "\n")
+end
+
+function pp_region_layout(root, wanted_keys)
+    default_keys = {
+        type=true, name=true
+        , managed=true
+        , split_tree=true
+        , tl=true, br=true
+    }
+    
+    wanted_keys = table.join(wanted_keys or {}, default_keys)
+    return table_to_string(root:get_configuration(), nil,
+                           { filter =
+                                 function(k, v)
+                                     return type(k) ~= "string" or wanted_keys[k]
+                                 end
+    })
+end
+
+-- end Debug
+
 --- Keybindings
 
 bindutils = {}
